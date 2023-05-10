101dc795b92a776444e119d44f7420ddb9bfa1c5
thatkhay <johnkingsleyegeonu@gmail.com>
2023-05-10 14:17:00 +0100

    created the UI

--- no-guesser.js
+++ no-guesser.js
@@ -1,2 +1,15 @@
 let min = 1;
 let max = 10;
+let winingNum = 2;
+let guessesLeft = 3;
+
+const game = document.querySelector('#game');
+const minNum = document.querySelector('#min-num');
+const maxNum = document.querySelector('#max-num');
+const guessBtn = document.querySelector('#guess-btn');
+const guessInput = document.querySelector('#guess-input');
+const message = document.querySelector('.message');
+
+
+
+
